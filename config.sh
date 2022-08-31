6c3aacc218654e3bee6c78797dd2248c9861f84c
dsevastianov <dsevastianov@users.noreply.github.com>
2022-08-31 15:03:03 -0400

    Scripting for secret generation

--- config.sh
+++ config.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 docker pull tootsuite/mastodon
-SECRET_KEY_BASE=$(docker run --rm tootsuite/mastodon bundle exec /opt/ruby/bin/rake secret)
-OTP_SECRET=$(docker run --rm tootsuite/mastodon bundle exec /opt/ruby/bin/rake secret)
-VAPID=$(docker run --rm tootsuite/mastodon bundle exec /opt/ruby/bin/rake mastodon:webpush:generate_vapid_key)
-echo "SECRET_KEY_BASE $SECRET_KEY_BASE"
-echo "OTP_SECRET $OTP_SECRET"
-echo "VAPID $VAPID"
+declare -A vars
+vars[SECRET_KEY_BASE]=$(docker run --rm tootsuite/mastodon bundle exec /opt/ruby/bin/rake secret)
+vars[OTP_SECRET]=$(docker run --rm tootsuite/mastodon bundle exec /opt/ruby/bin/rake secret)
+eval $(docker run --rm tootsuite/mastodon bundle exec /opt/ruby/bin/rake mastodon:webpush:generate_vapid_key) 
+vars[VAPID_PRIVATE_KEY]=$VAPID_PRIVATE_KEY 
+vars[VAPID_PUBLIC_KEY]=$VAPID_PUBLIC_KEY
+
+for key in "${!vars[@]}"; do
+  sed -i '' "s/$key=.*/$key=${vars[$key]}/g" ./.balena/secrets/.env.production
+done
